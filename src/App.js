637b14046b37b3207602887593fae539e416580e
Andy <wlo@fortinet.com>
2020-01-11 19:53:56 -0800

    The skeleton of the project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Navigation from './components/Navigation/Navigation'
+import Logo from './components/Logo/Logo'
+import Rank from './components/Rank/Rank'
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
+import Particles from 'react-particles-js';
 
+const particlesOptions = {
+  particles: {
+    number:{
+      value: 30,
+      density: {
+        enable: true,
+        value_area: 100
+      }
+    }
+  }
+}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Particles className='particles'
+              params={particlesOptions}
+            />
+      <Navigation />
+      <Logo />
+      <Rank />
+      <ImageLinkForm />
+      {/* <FaceRegonitop /> */}
+
     </div>
   );
 }
---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,18 @@
+
+import React from 'react';
+
+const Rank = () => {
+    return(
+        <div>
+            <div className='white f3 '>
+                {'Andy, Your current rank is...'}
+            </div>
+            <div className='white f1 '>
+                {'#4'}
+            </div>
+        </div>
+    )
+    
+}
+
+export default Rank;
